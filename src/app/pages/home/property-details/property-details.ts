8806cdb6b97931f8f11ee8ba41e09a016dd9722b
Riley <rmcaldwell2015@gmail.com>
2025-08-23 00:07:52 -0400

    Table updates

--- src/app/pages/home/property-details/property-details.ts
+++ src/app/pages/home/property-details/property-details.ts
@@ -3,6 +3,7 @@ import { Property } from '../../../models/property.model';
 import { PropertyService } from '../../../services/property.service';
 import { CommonModule } from '@angular/common';
 import { LucideIconsModule } from '../../../lucide.module';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-property-details',
@@ -12,9 +13,62 @@ import { LucideIconsModule } from '../../../lucide.module';
     LucideIconsModule,
   ],
   templateUrl: './property-details.html',
-  styleUrls: ['./property-details.css']
+  styleUrls: ['./property-details.css'],
+  animations: [
+    trigger('expandCollapse', [
+      state('collapsed', style({ height: '0px', opacity: 0, overflow: 'hidden' })),
+      state('expanded', style({ height: '*', opacity: 1 })),
+      transition('collapsed <=> expanded', animate('300ms ease'))
+    ])
+  ]
 })
 export class PropertyDetailsComponent implements OnInit {
+
+  units = [
+    {
+      floorUnitNumber: "1st Floor - 350 E1",
+      use: "Industrial",
+      size: 12000,
+      rate: 24,
+      status: "AVAILABLE",
+      rowClass: "table-row available",
+      statusClass: "status-available",
+      expanded: false
+    },
+    {
+      floorUnitNumber: "1st Floor - Ste J",
+      use: "Office",
+      size: 45000,
+      rate: 13,
+      status: "AVAILABLE",
+      rowClass: "table-row available",
+      statusClass: "status-available",
+      expanded: false
+    },
+    {
+      floorUnitNumber: "1st Floor - M",
+      use: "Office",
+      size: 81000,
+      rate: 13,
+      status: "COMING SOON",
+      rowClass: "table-row available",
+      statusClass: "status-coming-soon",
+      expanded: false
+    },
+    {
+      floorUnitNumber: "2nd Floor - Ste 227",
+      use: "Office",
+      size: 45000,
+      rate: 13,
+      status: "AVAILABLE",
+      rowClass: "table-row coming-soon",
+      statusClass: "status-available",
+      expanded: false
+    }
+  ]
+
+
+
   property: Property | null = null;
 
   constructor(private propertyService: PropertyService) { }
